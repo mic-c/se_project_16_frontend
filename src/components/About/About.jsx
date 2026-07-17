69e354820263b0f9ff562f4fa72c49a538fc82b0
mic-c <williamsarimah@gmail.com>
2026-07-17 14:14:14 -0500

    Refine About component BEM element naming

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,10 +5,10 @@ export default function About() {
   return (
     <section className="about">
       <div className="about__container">
-        <div className="about__image-wrapper">
+        <div className="about__image-block">
           <div className="about__author-photo" aria-label="Author photo" />
         </div>
-        <div className="about__text-wrapper">
+        <div className="about__text-block">
           <h2 className="about__title">About the Author</h2>
           <p className="about__text">
             Hi, I’m Chukwuka. I built News Explorer to help people discover and
